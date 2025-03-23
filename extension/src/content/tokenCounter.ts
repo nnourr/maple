64fab743ecec9fed84f729eb22f1b14617d79ac9
nnourr <nourqwerty123@gmail.com>
2025-03-23 07:27:51 -0400

    something or another

--- extension/src/content/tokenCounter.ts
+++ extension/src/content/tokenCounter.ts
@@ -1,11 +1,35 @@
-chrome.storage.local.get("tokens", (result) => {
-  let tokens = result.tokens;
-  if (tokens) {
-    tokens += 15;
-  } else {
-    tokens = 15;
+function updateTokenCount() {
+  if (window.location.href.includes("search")) {
+    chrome.storage.local.get("tokens", (result) => {
+      let tokens = result.tokens;
+      if (tokens) {
+        tokens += 15;
+      } else {
+        tokens = 15;
+      }
+      chrome.storage.local.set({ tokens: tokens }, () => {
+        console.log("[TokenCounter] Tokens is set to " + tokens);
+      });
+    });
   }
-  chrome.storage.local.set({ tokens: tokens }, () => {
-    console.log("[TokenCounter] Tokens is set to " + tokens);
-  });
+}
+
+updateTokenCount();
+
+let prevUrl = window.location.href;
+const observer = new MutationObserver(() => {
+  console.log("[TokenTracker] MutationObserver callback fired");
+  console.log("[TokenTracker] Current URL: " + window.location.href);
+  console.log("[TokenTracker] prevUrl URL: " + prevUrl);
+
+  const url = window.location.href;
+  if (url !== prevUrl) {
+    updateTokenCount();
+    prevUrl = url;
+  }
+});
+
+observer.observe(document.body, {
+  childList: true,
+  subtree: true,
 });
